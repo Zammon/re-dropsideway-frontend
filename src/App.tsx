bb6e15c1da41c4a79cf3878c3d761fd60e3a90d1
Zammon <hongnakub12@gmail.com>
2025-01-01 16:52:23 +0700

    [UPDATE] instatlll axios, redux, react router and setup react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import logo from './logo.svg';
+import './styles/index.scss';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,5 +22,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
